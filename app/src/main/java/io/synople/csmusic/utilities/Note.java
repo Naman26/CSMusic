caddd73ba44241f219b30d60f93e9b42e0d7d210
carlwilhjelm <carlwilhjelm@gmail.com>
2019-01-26 01:12:52 -0500

    Update Note
    added file name and fixed some obvious bugs

--- app/src/main/java/io/synople/csmusic/utilities/Note.java
+++ app/src/main/java/io/synople/csmusic/utilities/Note.java
@@ -14,19 +14,30 @@ public class Note {
     String[] keyG = {"G", "A", "B", "C", "D", "E", "F#", "G"};
 
     //all keys
-
     String[][] keys = {keyC, keyD, keyG};
 
+    //all octaves
+    String[] octStrings = {"3", "4", "5", "6"};
+
+    //all lengths
+    String[] countStrings = {"i", "q", "h", "w"};
+
     private int key, nameNum, oct, counts;
+
+    // filename format is Name + Number + Count + .wav
     private String fileName;
     private final String wav = ".wav";
 
     public Note() {
         Random rand = new Random();
         key = rand.nextInt(3);
-        nameNum = rand.nextInt(7);
+        nameNum = rand.nextInt(8);
         oct = rand.nextInt(3);
-        counts = rand.nextInt(3);
+        if (nameNum == 8){
+            oct++;
+        }
+        counts = rand.nextInt(4);
+        fileName = setFileName(key, nameNum, oct, counts);
     }
 
     public Note(int keyIn, int nameNumIn, int octIn, int countsIn){
@@ -34,8 +45,16 @@ public class Note {
         nameNum = nameNumIn;
         oct = octIn;
         counts = countsIn;
-    }
-
-
+        fileName = setFileName(key, nameNum, oct, counts);
 
     }
+
+    public String getFileName(){
+        return fileName;
+    }
+
+    public String setFileName(int keyIn, int nameNumIn, int octIn, int countsIn) {
+        return keys[keyIn][nameNumIn] + octStrings[octIn] + countStrings[countsIn];
+
+    }
+}
