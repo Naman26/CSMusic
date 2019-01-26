cb3fef26434a7b8c8a1c03294377b6fbe07f7697
carlwilhjelm <carlwilhjelm@gmail.com>
2019-01-26 00:39:35 -0500

    Note class added

---
+++ app/src/main/java/io/synople/csmusic/utilities/Note.java
@@ -0,0 +1,41 @@
+package io.synople.csmusic.utilities;
+
+import java.util.Random;
+
+public class Note {
+
+    //Key of C
+    String[] keyC = {"C", "D", "E", "F", "G", "A", "B", "C"};
+
+    //Key of D
+    String[] keyD = {"D", "E", "F#", "G", "A", "B", "C#", "D"};
+
+    //Key of G
+    String[] keyG = {"G", "A", "B", "C", "D", "E", "F#", "G"};
+
+    //all keys
+
+    String[][] keys = {keyC, keyD, keyG};
+
+    private int key, nameNum, oct, counts;
+    private String fileName;
+    private final String wav = ".wav";
+
+    public Note() {
+        Random rand = new Random();
+        key = rand.nextInt(3);
+        nameNum = rand.nextInt(7);
+        oct = rand.nextInt(3);
+        counts = rand.nextInt(3);
+    }
+
+    public Note(int keyIn, int nameNumIn, int octIn, int countsIn){
+        key = keyIn;
+        nameNum = nameNumIn;
+        oct = octIn;
+        counts = countsIn;
+    }
+
+
+
+    }
